e63542f2719ef9adaaabec0c92918e8fd7abd971
Yaroslav Novitski <yaroslav.auto@gmail.com>
2024-09-16 13:33:40 +0300

    add lab1

---
+++ app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from data import PARTS_DATA
+from draft_order import DRAFT_ORDER
+
+def index(request):
+    part_name = request.GET.get("part_name", "")  # Use a more descriptive name for the query parameter
+    parts = search_part(part_name)  # Search for parts matching the part name
+    parts_in_processing = sum(1 for part in DRAFT_ORDER['parts'])
+    return render(request, 'index.html', {
+        'parts': parts, 
+        'part_name': part_name,  # Pass the part name to the template
+        'DRAFT_ORDER': DRAFT_ORDER,
+        'parts_in_processing': parts_in_processing  # Number of parts in processing
+    })
+
+def part(request, part_id):
+    part = get_part_by_id(part_id)
+    if not part:
+        return render(request, '404.html', status=404)
+    return render(request, "part.html", {'part': part})
+
+def orders(request, order_id):
+    order = getOrderById(order_id)  # Получаем заказ
+    delivery_parts = [part for part in order["parts"] if part["operation"] == "delivery"]
+    shipment_parts = [part for part in order["parts"] if part["operation"] == "shipment"]
+
+    # Проверяем наличие POST запроса от кнопки "Оформить"
+    if request.method == "POST":
+        return render(request, "orders.html", {"DRAFT_ORDER": order, "parts": shipment_parts, "operation_type": "shipment"})
+    
+    # Если есть доставки, выводим их, иначе сразу отгрузки
+    if delivery_parts:
+        return render(request, "orders.html", {"DRAFT_ORDER": order, "parts": delivery_parts, "operation_type": "delivery"})
+    else:
+        return render(request, "orders.html", {"DRAFT_ORDER": order, "parts": shipment_parts, "operation_type": "shipment"})
+    
+
+def get_part_by_id(part_id):
+    for part in PARTS_DATA:
+        if part['id'] == part_id:
+            return part
+    return None
+
+def search_part(part_name):
+    res = []
+    for part in PARTS_DATA:
+        if part_name.lower() in part["name"].lower():
+            res.append(part)
+    return res
+
+def getOrderById(order_id):
+    return DRAFT_ORDER
---
+++ draft_order.py
@@ -0,0 +1,33 @@
+DRAFT_ORDER = {
+    "id": 123,
+    "planned_date": '30.09.2024',
+    "parts": [
+        {
+            'id': 1,
+            'image' : 'http://localhost:9000/web/parts/1.jpg',
+            'name': 'ENGINE SHORT',
+            'specification': 'V8 EFI PETROL',
+            'oem_number': 'TF-EXH-2024-001',
+            'operation' : 'shipment',
+            'quantity': 1,
+        },
+        {
+            'id': 2,
+            'image' : 'http://localhost:9000/web/parts/2.jpg',        
+            'name': 'TRANSMISSION BRAKE',
+            'specification': 'V8 EFI PETROL',
+            'oem_number': 'QX-EXH-2024-005',
+            'operation' : 'delivery',
+            'quantity': 5,
+        },
+        {
+            'id': 3,
+            'image' : 'http://localhost:9000/web/parts/3.jpg',        
+            'name': 'TAILPIPE',
+            'specification': 'V8 EFI PETROL',
+            'oem_number': 'ST-EXH-2024-004',
+            'operation' : 'delivery',
+            'quantity': 4,
+        },
+    ]
+}
